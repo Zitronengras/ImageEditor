ce3cb8c24c83c43e9c8fd360fa5fed2ef04f2c0a
Magnifica <magnifica92@hotmail.de>
2014-04-16 15:25:56 +0200

    Frame mit Main

--- ImageEditor.java
+++ ImageEditor.java
@@ -1,7 +1,15 @@
+import javax.swing.JFrame;
+
 public class ImageEditorFrame extends JFrame{ 
- public ImageEditorFrame(){ 
- setDefaultCloseOperation(EXIT_ON_CLOSE); 
- setSize(400, 300); 
- setVisible(true); 
- } 
+	public ImageEditorFrame(){ 
+	setDefaultCloseOperation(EXIT_ON_CLOSE); 
+	setSize(400, 300); 
+	setVisible(true); 
+	} 
 } 
+
+public class Main{
+	public static void main (String []args){
+	ImageEditorFrame frm = new ImageEditorFrame();
+	}
+}
